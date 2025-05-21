42ba753d4f25ad05d7a7a1cbcd42332ba5b188ed
JavaCommons Technologies <javacommons@gmail.com>
2025-05-22 07:40:15 +0900

    .

--- std/pull.sh
+++ std/pull.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-cname=javacommons/paperspace-std:v2025.0522.0600-JST
+cname=javacommons/paperspace-std:v2025.0522.0720-JST
 docker pull $cname
 docker images
--- std/run.sh
+++ std/run.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-cname=javacommons/paperspace-std:v2025.0522.0600-JST
+cname=javacommons/paperspace-std:v2025.0522.0720-JST
 docker run -v `cygpath -w ~/_/docker/docker-data`:/docker-data -it $cname //bin/bash
