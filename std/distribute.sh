1c96474d0b33c9c493db28e8b027f98173adcdfb
JavaCommons Technologies <javacommons@gmail.com>
2025-05-22 04:24:01 +0900

    .

---
+++ std/distribute.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+set -uvx
+set -e
+docker commit 0418f059a360 javacommons/pro-drlhi:v2025.0522.0323
+docker commit 0418f059a360 javacommons/pro-kadoma:v2025.0522.0323
+docker commit 0418f059a360 javacommons/pro-jojojojomj:v2025.0522.0323
+docker commit 0418f059a360 javacommons/pro-potsu:v2025.0522.0323
+docker commit 0418f059a360 javacommons/pro-kohmie:v2025.0522.0323
