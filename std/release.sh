e083a63d08c67aadeb23b39cd6982eebfbbad82d
JavaCommons Technologies <javacommons@gmail.com>
2025-05-22 14:55:00 +0900

    .

--- std/release.sh
+++ std/release.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-cname=javacommons/framepack-pro:v2025.0522.1308
+version=v2025.0522.1308
+cname=javacommons/framepack-pro:$version
+docker pull $cname
 docker run -v `cygpath -w ~/_/docker/docker-data`:/docker-data -it $cname //bin/bash
