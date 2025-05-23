cf6494ae5bc2d17eddd78b13e2ca13251982fe29
JavaCommons Technologies <javacommons@gmail.com>
2025-05-23 20:42:22 +0900

    .

--- std/run.sh
+++ std/run.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-cname=javacommons/paperspace-std:v2025.0522.0720-JST
+cname=javacommons/paperspace-std:v2025.0523.2009-JST
 docker pull $cname
 docker run -v `cygpath -w ~/_/docker/docker-data`:/docker-data -it $cname //bin/bash
