bb28f86f30f9c7b4d5a3aa9573957ad469c4323e
JavaCommons Technologies <javacommons@gmail.com>
2025-05-23 21:06:40 +0900

    .

--- std-lastest/pull.sh
+++ std-lastest/pull.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-docker pull nvidia/cuda:12.6.0-devel-ubuntu22.04
+#docker pull nvidia/cuda:12.6.0-devel-ubuntu22.04
+docker pull nvidia/cuda:12.6.0-devel-ubuntu24.04
 docker images
--- std-lastest/run.sh
+++ std-lastest/run.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-cname=javacommons/paperspace-std:v2025.0523.2009-JST
+cname=javacommons/paperspace-std:v2025.0523.2106-JST
 docker run -v `cygpath -w ~/_/docker/docker-data`:/docker-data -it $cname //bin/bash
