6ed778775ff9e4d79b8ff3a55beb29fbbb08bdf0
JavaCommons Technologies <javacommons@gmail.com>
2025-05-23 17:50:16 +0900

    .

--- std-lastest/mk.sh
+++ std-lastest/mk.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-cname=javacommons/paperspace-std:v2025.0523.1705-JST
+cname=javacommons/paperspace-std:v2025.0523.1750-JST
 docker builder prune --force
 docker build -t $cname .
--- std-lastest/run.sh
+++ std-lastest/run.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-cname=javacommons/paperspace-std:v2025.0523.1705-JST
+cname=javacommons/paperspace-std:v2025.0523.1750-JST
 docker run -v `cygpath -w ~/_/docker/docker-data`:/docker-data -it $cname //bin/bash
