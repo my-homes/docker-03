b15225a0209e55d6ce494102354392a54556ade7
JavaCommons Technologies <javacommons@gmail.com>
2025-05-23 17:05:36 +0900

    .

--- std-lastest/mk.sh
+++ std-lastest/mk.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-cname=javacommons/paperspace-std:v2025.0523.1646-JST
+cname=javacommons/paperspace-std:v2025.0523.1705-JST
 docker builder prune --force
 docker build -t $cname .
