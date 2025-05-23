62d44fecb202795dbf1846a677952bdfc1e8d574
JavaCommons Technologies <javacommons@gmail.com>
2025-05-23 20:48:59 +0900

    .

--- std/distribute.sh
+++ std/distribute.sh
@@ -3,7 +3,7 @@ set -uvx
 set -e
 id=756ba56c899c
 version=v1.9.4.0
-docker commit $id javacommons/pro-manbosan:$version
+docker commit $id javacommons/pro-siomusubi:$version
 exit 0
 docker commit $id javacommons/pro-drlhi:$version
 docker commit $id javacommons/pro-kadoma:$version
@@ -11,3 +11,4 @@ docker commit $id javacommons/pro-jojojojomj:$version
 docker commit $id javacommons/pro-potsu:$version
 docker commit $id javacommons/pro-kohmie:$version
 docker commit $id javacommons/pro-tetsu292929:$version
+docker commit $id javacommons/pro-manbosan:$version
